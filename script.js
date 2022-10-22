bfe7184f2b63828573eb713feeba5666f35338b9
Yohannes14 <yohansdegu.2010@gmail.com>
2022-10-22 11:29:46 -0400

    map iteration

--- script.js
+++ script.js
@@ -47,34 +47,67 @@ const restaurant = {
     }
 };
 
+////////////////////////////////////////////////////////
+//Map iteration
+ const question = new Map([
+    ['question','What is the best programming language i nthe world'],
+    [1, 'C'],
+    [2, 'Java'],
+    [3, 'JavaScript'],
+    ['correct', 3],
+    [true, 'Correct'],
+    [false, 'Try again'],
+ ]);
+ console.log(question);
+
+ //Convert object to map
+ console.log(Object.entries(openingHours));
+ const hoursMap =new Map(Object.entries(openingHours));
+ console.log(hoursMap) 
+ for(const [key ,value] of question){
+    if(typeof key ==='number') console.log(`Answer ${key}: ${value}`)
+
+ }
+ //const answer =Number(prompt('Your answer'));
+ const answer =3;
+ console.log(answer)
+
+console.log(question.get(question.get('correct')===answer))
+ 
+//Convert map to array
+console.log([...question]);
+//console.log(question.entries());
+console.log([...question.keys()]);
+console.log([...question.values()]);
+
 //////////////////////////////////////////////////////////
 //Maps Fundamentals
-const rest =new Map();
-rest.set('name', 'Classico Itaiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, portugal'));
+//const rest =new Map();
+// rest.set('name', 'Classico Itaiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, portugal'));
 
-rest.set('categories', ['Itslian', 'pizzeria', 'vegetarian', 'organic'])
-     .set('open', 11)
-     .set('close', 23)
-     .set('true', 'We are open:D')
-     .set('false', 'We are closed : (');
-// console.log(rest); 
+// rest.set('categories', ['Itslian', 'pizzeria', 'vegetarian', 'organic'])
+//      .set('open', 11)
+//      .set('close', 23)
+//      .set('true', 'We are open:D')
+//      .set('false', 'We are closed : (');
+// // console.log(rest); 
 
-console.log(rest.get('name'));
-console.log(rest.get('true'));
-console.log(rest.get(1));
+// console.log(rest.get('name'));
+// console.log(rest.get('true'));
+// console.log(rest.get(1));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-const arr =[1, 2];
-rest.set(arr, 'Test');
-rest.set(document.querySelector('h1', 'heading'))
-console.log(rest)
-// rest.clear();
-console.log(rest.size);
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// const arr =[1, 2];
+// rest.set(arr, 'Test');
+// rest.set(document.querySelector('h1', 'heading'))
+// console.log(rest)
+// // rest.clear();
+// console.log(rest.size);
 
-console.log(rest.get(arr));
+// console.log(rest.get(arr));
 
 
 
