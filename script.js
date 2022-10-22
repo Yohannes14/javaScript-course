a3e7628a2afa438f585d858d4cd9ebb3df3fa9a6
Yohannes14 <yohansdegu.2010@gmail.com>
2022-10-21 20:20:08 -0400

    Coding challenge #1

--- script.js
+++ script.js
@@ -40,17 +40,125 @@ const restaurant = {
         console.log(mainIngredient, otherIngredient)
     }
 };
-///Logical Assignment Operators
- const rest1 ={
-    name:"John",
-    // numGuests: 20,
-    numGuests: 0,
- };
 
- const rest2 ={
-    name: 'la Piazza',
-    owner: 'Giovanni Rossi',
- };
+/// Looping Array
+
+// const menu =[...restaurant.starterMenu, ...restaurant.mainMenu];
+// for(const item of menu) console.log(item);
+
+// // for(const item of menu.entries()){
+// //     console.log(`${item[0] +1}:${item[1]}`);
+// // }  
+// for(const [i, el] of menu.entries()){
+//     console.log(`${i +1}:${el}`);
+// }     
+// console.log(...menu.entries());
+
+/////////////////////////////////////////////
+//Coding Challeng #1
+/* 
+ We're building a football betting app (soccer for my) American friends)
+
+ Suppose we got data from web service about a certain game(below). 
+ in this challenge we're gonna work with the data. So here are your tasks:
+
+  1.Create one player array for each team (variables 'player1' and 'player2')
+  2. The first player in any player array is the goalkeeper and the others are field players.
+    For Bayern Munich (team 1) create one variable ('gk')
+    with the goalkeeper's name, and one array ('fieldPlayers') with all the remainig 10 field
+    players
+  3. Create an array 'allPlayers' containing all players of both teams (22 players)
+  4. During the game, bayern Munich (team 1) used 3 substitute players. So create a new array ('playersFinal')
+     containig all the original team1 players plus 'Thiago', 'Coutinho' and 'Perisit'
+  5. Based on the game.odds object, create one variable for each odd (called 'team1', 'draw' and 'team2')
+  6. Write a function ('printGoals') that receives an arbitrary number of player names (NOT an array)
+     and prints each of them to the console, along with the number of goals who were scored
+     (number of player names passed in)
+  7. The team with the lower odd is more likely to win, print to the console which team is more likely to win, Without using an if/else statement or the ternary operator.
+
+   TEST DATA FOR 6: Use players 'Davies', 'Muller',
+     'Lewandowski' and 'Kimmich'. Then, call the function again with players from game.scored
+   */
+
+     const game = {
+        team1: 'Bayern Munich',
+        team2: 'Borrussia Dortmund',
+        players: [
+        [
+        'Neuer',
+        'Pavard',
+        'Martinez',
+        'Alaba',
+        'Davies',
+        'Kimmich',
+        'Goretzka',
+        'Coman',
+        'Muller',
+        'Gnarby',
+        'Lewandowski',
+        ],
+        [
+        'Burki',
+        'Schulz',
+        'Hummels',
+        'Akanji',
+        'Hakimi',
+        'Weigl',
+        'Witsel',
+        'Hazard',
+        'Brandt',
+        'Sancho',
+        'Gotze',
+        ],
+        ],
+        score: '4:0',
+        scored: ['Lewandowski', 'Gnarby', 'Lewandowski',
+        'Hummels'],
+        date: 'Nov 9th, 2037',
+        odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
+        },
+        };
+//1
+const [player1,player2] = game.players;
+//2
+const [gk, ...fieldPlayers] = player1;
+//3
+const allPlayers= [...player1, ...player2];
+//4
+const playersFinal =[...player1, 'Thiago', 'Coutinho' ,'Perisit'];
+//5
+
+const {odds:{team1, x:draw, team2}} =game;
+console.log(team1, draw, team2)
+
+//6
+
+const printGoals =function(...player){
+     console.log(`${player.length} were scored`)
+}
+ 
+printGoals(...game.scored);  
+
+//7
+ team1 <team2 && console.log('Team 1 is more likely to win');
+ team2 <team1 && console.log('Team 2 is more likely to win');
+
+
+/////////////////////////////////
+///Logical Assignment Operators
+//  const rest1 ={
+//     name:"John",
+//     // numGuests: 20,
+//     numGuests: 0,
+//  };
+
+//  const rest2 ={
+//     name: 'la Piazza',
+//     owner: 'Giovanni Rossi',
+//  };
 // rest1.numGuests = rest1.numGuests || 10;
 // rest2.numGuests = rest2.numGuests || 10;
 //console.log(rest1, rest2);
@@ -65,9 +173,9 @@ const restaurant = {
 //AND assignment operator
 // rest1.owner = rest1.owner && '<ANONYMOUS'
 // rest2.owner = rest2.owner && '<ANONYMOUS'
-rest1.owner &&= '<ANONYMOUS'
-rest2.owner &&= '<ANONYMOUS'
-console.log(rest1, rest2);
+// rest1.owner &&= '<ANONYMOUS'
+// rest2.owner &&= '<ANONYMOUS'
+// console.log(rest1, rest2);
 
 
 ////////////// Nullish Coalescing operator(__)
