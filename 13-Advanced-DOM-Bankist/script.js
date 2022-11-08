aca624cd8b37c83560e7f231d2777124fd396925
Yohannes14 <yohansdegu.2010@gmail.com>
2022-11-08 13:21:11 -0500

    AJAX Call_ XMLHttpRequest

---
+++ 13-Advanced-DOM-Bankist/script.js
@@ -0,0 +1,159 @@
+'use strict';
+
+///////////////////////////////////////
+// Modal window
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.btn--close-modal');
+const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+/*
+/////////////////////////////////////////////////////////
+// Selecting, 
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+
+const header =document.querySelector('.header');
+const allSection = document.querySelectorAll(".section");
+console.log(allSection);
+
+document.getElementById('section--1');
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);
+
+console.log(document.getElementsByClassName('btn'));
+
+////Creating and inserting Elements
+
+const message =document.createElement('div');
+message.classList.add('cookie-message');
+// message.textContent = 'We use cookied for improved functionality and analytics.';
+message.innerHTML = 'We use cookied for improved functionality and analytics. <button class="btn btn--close-cookie"> Got it!</button>';
+
+// header.prepend(message);
+header.append(message);
+// header.append(message.cloneNode(true))
+
+// header.before(message);
+// header.after(message);
+
+/// Delete elements
+document.querySelector('.btn--close-cookie').
+addEventListener('click', function(){
+  // message.remove();
+  message.parentElement.removeChild(message);
+
+});
+/////////////////////////////////////////
+// styles
+ message.style.backgroundColor = '#37383d';
+ message.style.width = '120%';
+ //working inline style
+ console.log(message.style.height);
+ console.log(message.style.backgroundColor);
+
+ console.log(getComputedStyle(message));
+ console.log(getComputedStyle(message).color);
+ console.log(getComputedStyle(message).height);
+
+ document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+ //Attributes
+ const logo = document.querySelector('.nav__logo');
+ console.log(logo.alt);
+ console.log(logo.src);
+ console.log(logo.className);
+ logo.setAttribute('company', 'Bankist');
+
+ console.log(logo.src);
+ logo.getAttribute('src'); //relative
+
+ const link = document.querySelector('.nav__link--btn');
+  console.log(link.href);
+  console.log(link.getAttribute('href'));
+
+  // Data attributes
+  console.log(logo.dataset.versionNumber)
+
+
+  //Classes
+  logo.classList.add('c');
+  logo.classList.remove('c');
+  logo.classList.contains('c') // not includes
+
+  //Don't use
+  logo.className = 'Jhon';
+  */
+
+  const btnScrollTo = document.querySelector('.btn--scroll-to');
+  const section1 = document.querySelector('#section--1');
+
+  btnScrollTo.addEventListener('click', function(e) {
+    const s1coords = section1.getBoundingClientRect();
+    console.log(s1coords);
+    console.log(e.target.getBoundingClientRect());
+    // current scroll
+    console.log('Current scroll (X/Y', window.pageXOffset, window.pageYOffset);
+    console.log('height/width viewport', 
+    
+         document.documentElement.clientHeight,
+         document.documentElement.clientWidth
+      );
+
+      //Scrolling
+      // window.scrollTo(s1coords.left + window.pageXOffset, 
+      //   s1coords.top + window.pageYOffset
+      //   );
+
+      // window.scrollTo({
+      //   left: s1coords.left + window.pageXOffset, 
+      //   top: s1coords.top + window.pageYOffset,
+      //   behavior: 'smooth',
+
+      // });
+      section1.scrollIntoView({bahavior: 'smooth'})
+  });
+
+  //////////////////////////////////////////
+  //Types of event handlers
+  const h1 =document.querySelector('h1');
+
+  const alertH1 =function(e) {
+    alert('addEventListener: Great! You are reading the headin: D');
+
+    h1.removeEventListener('mouseenter', alertH1);
+
+  };
+
+  h1.addEventListener('mouseenter', alertH1)
+
+  setTimeout(() => h1.removeEventListener('mouseenter'. alertH1), 3000);
+
+  // h1.onmouseenter = function(e) {
+  //   alert('onmouseenter: Great! You are reading the headin: D')
+  // };
--- script.js
+++ script.js
@@ -1,134 +1,36 @@
-//Importing module
-// import {addToCart, totalPrice as tp, totalQuantity} from './shopingCart.js';
-// addToCart('bread', 6); 
-// console.log(tp, totalQuantity)
+'use strict';
 
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
 
+///////////////////////////////////////
 
-// import * as ShoppingCart from "./shopingCart.js"
-// console.log('Exporting module');
-// ShoppingCart.addToCart('bread', 6);
-// console.log(ShoppingCart.totalPrice);
+const getCountryData = function (country){
+const request = new XMLHttpRequest();
+request.open('GET', `https://restcountries.com/v2/name/${country}`);
+request.send();
 
-import add, {cart} from "./shopingCart.js";
-add('pizza', 4)
-add('pizza', 4)
-add('pizza', 7)
-add('pizza', 6)
-console.log(cart)
-///////////////////////////////////////////////////
-//top level await
+request.addEventListener('load', function(){
 
-// const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await response.json();
-// console.log(data);
-
-
-const getLastPost = async function() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
-    const data = await response.json();
-    return {title:data.at(-1).title, text:data.at(-1).body};
-}
-
-const lastPost = getLastPost();
-console.log(lastPost);
-
-// Not very clean
-// lastPost.then(last => console.log(last));
-
-// Top level await
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
-
-/////////////////////////////////////////////////////////
-// The module pattern
-const ShoppingCart2 = (function() {
- 
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
-    const addToCart = function(product, quantity) {
-        cart.push({product, quantity});
-        console.log(`${quantity} ${product} added to cart
-        (shipping cost is ${shippingCost})`)
-    };
-    const orderStock = function(product, quantity) {
-        console.log(`${quantity} ${product} ordered from supplier`);
-    };
-
-    return {
-        addToCart,
-        orderStock,
-        cart,
-        totalPrice,
-        totalQuantity,
-    };
-})();
-
-ShoppingCart2.addToCart('apple', 4);
-ShoppingCart2.addToCart('apple', 6);
-console.log(ShoppingCart2);
-
-// console.log(ShoppingCart2.shippingCost); // not working
-
-///////////////////////////////////////////////////////////
-/*
-//CommonJS Modules
-//Export
-export.addToCart = function(product, quantity) {
-    cart.push({product, quantity});
-    console.log(`${quantity} ${product} added to cart
-    (shipping cost is ${shippingCost})`)
+    const data =JSON.parse(this.responseText);
+  
+    const html =`
+    <article class="country">
+    <img class="country__img" src="${data[0].flag}"/>
+    <div class="country__data">
+      <h3 class="country__name">${data[0].name}</h3>
+      <h4 class="country__region">${data[0].region}</h4>
+      <p class="country__row"><span>👫</span>${(+data[0].population/1000000).toFixed(1)
+      }</p>
+      <p class="country__row"><span>🗣️</span>${data[0].languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data[0].currencies[0].name
+      }</p>
+    </div>
+  </article>`;
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+});
 };
 
-//Import 
-const {addToCart} =require('./shopingCart.js');
-*/
-///////////////////////////////////////////////////////////
-import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
-
-const state ={
-    cart: [
-        {product: 'bread', quantity: 6},
-        {product: 'pizza', quantity: 5},
-    ],
-    user :{loggedIn: true},
-};
-
-const stateClone = Object.assign({}, state);
-
-const stateDeepClone = cloneDeep(state);
-stateClone.user.loggedIn = false;
-
-console.log(stateClone);
-console.log(stateDeepClone);
-
-// if(module.hot) {
-//     module.hot.accept()
-// }
-
-//////////////////////////////////////////////
-//Configuring Babel and Polyfilling
-class Person {
-    #greeting = 'Hey'
-    constructor(name) {
-        this.name = name;
-        console.log(`${this.#greeting}, ${this.name}`)
-    }
-}
-const jhon = new Person('Jhon');
-
-console.log('Jhon' ?? null);
-
-console.log(cart.find(el => el.quantity >= 2));
-
-Promise.resolve('Test').then(x => console.log(x));
-
-import 'core-js/stable';
-// import 'core-js/stable/array/find'
-
-// Polifilling async function
-import 'regenerator-runtime/runtime'
-
-
+getCountryData('ethiopia');
+getCountryData('usa');
